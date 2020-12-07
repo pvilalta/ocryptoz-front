437cae0e0d6cada39af0314b63ed905d9e20943f
Paul Vilalta <x2cheese@MacBook-Pro-de-Paul.local>
2020-12-07 18:07:09 +0100

    frist try

---
+++ src/components/App/index.js
@@ -0,0 +1,16 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import GetStarted from 'src/components/GetStarted';
+import './styles.css';
+
+// == Composant
+const App = () => (
+  <div className="app">
+    <GetStarted />
+  </div>
+);
+
+// == Export
+export default App;
